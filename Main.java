741d88ef157ffe40deb12cd19a163f446ea8c0da
Karthik <karthikcheeraboyina@gmail.com>
2024-11-02 15:34:07 +0530

    First Commit.

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String [] z){
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Your name is " +name);
+    }
+}
